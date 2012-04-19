bdd01e6d3fe6800b30850670f1678cc5a0b7666b
jmiguelsamper <jmiguelsamper@ea36c20c-9e4e-4f5d-8fce-a88eceddb49c>
2012-04-19 15:45:48 +0000

    Example webapp

--- examples/src/main/java/net/sourceforge/pagesdialect/examples/MainController.java
+++ examples/src/main/java/net/sourceforge/pagesdialect/examples/MainController.java
@@ -1,4 +1,4 @@
-package org.thymeleaf.pagesdialect.examples;
+package net.sourceforge.pagesdialect.examples;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
