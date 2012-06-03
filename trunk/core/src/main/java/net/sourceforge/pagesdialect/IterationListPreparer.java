c0beafa87447502a97e4253dd6646b2b11e3aed3
jmiguelsamper <jmiguelsamper@ea36c20c-9e4e-4f5d-8fce-a88eceddb49c>
2012-06-03 12:38:39 +0000

    Fixed but at Exporting

--- trunk/core/src/main/java/net/sourceforge/pagesdialect/IterationListPreparer.java
+++ trunk/core/src/main/java/net/sourceforge/pagesdialect/IterationListPreparer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import org.springframework.beans.support.PagedListHolder;
 import org.thymeleaf.Arguments;
 import org.thymeleaf.context.IWebContext;
 import org.thymeleaf.dom.Element;
@@ -13,7 +12,7 @@ import org.thymeleaf.standard.expression.StandardExpressionProcessor;
 import org.thymeleaf.standard.processor.attr.StandardEachAttrProcessor;
 
 /**
- * Utility class to build a PagedListHolder list from the iteration object.
+ * Utility class to build a RecoverablePagedListHolder list from the iteration object.
  */
 public class IterationListPreparer {
 
@@ -29,28 +28,28 @@ public class IterationListPreparer {
 
     /**
      * Search for iteration expression.
-     * If iteration object is not PagedListHolder, replace it.
+     * If iteration object is not RecoverablePagedListHolder, replace it.
      */
-    public PagedListHolder findOrCreateIterationList() {
+    public RecoverablePagedListHolder findOrCreateIterationList() {
         String iterationExpression = findIterationExpression(arguments, elementContainingIteration);
         String iterationAttributeParams[] = elementContainingIteration.getAttributeValue(iterationExpression).split(":");
         String itemObject = iterationAttributeParams[0].trim();
         String listObject = iterationAttributeParams[1].trim();
-        // Check if PagedListHolder has been already created
+        // Check if RecoverablePagedListHolder has been already created
         IWebContext context = (IWebContext) arguments.getContext();
         if (context.getRequestAttributes().containsKey(PAGED_LIST_HOLDER_ATTR)) {
-            return (PagedListHolder) context.getRequestAttributes().get(PAGED_LIST_HOLDER_ATTR);
+            return (RecoverablePagedListHolder) context.getRequestAttributes().get(PAGED_LIST_HOLDER_ATTR);
         } else {
-            // Create and store PagedListHolder.
+            // Create and store RecoverablePagedListHolder.
             Object originalIterable = StandardExpressionProcessor.processExpression(arguments, listObject);
             // Replace original with paged list
-            PagedListHolder pagedListHolder = new PagedListHolder(convertToList(originalIterable));
+            RecoverablePagedListHolder pagedListHolder = new RecoverablePagedListHolder(convertToList(originalIterable));
             context.getRequestAttributes().put(PAGED_LIST_HOLDER_ATTR, pagedListHolder);
             elementContainingIteration.setAttribute(iterationExpression, itemObject + " : ${#ctx.requestAttributes." + PAGED_LIST_HOLDER_ATTR + ".pageList}");
             return pagedListHolder;
         }    
     }
-
+    
     /**
      * Find th:each attribute value inside the iterating element.
      */
